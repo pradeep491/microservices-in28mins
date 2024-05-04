9fd05f49f21aa16061462f1ea16d2b910b2be677
pradeep <k.pradeep0491@gmail.com>
2024-05-04 22:46:40 +0530

    changes

---
+++ api-gateway-loggingFilter/src/main/java/com/in28minutes/microservice/apigateway/ApiGatewayLoggingFilterApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservice.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayLoggingFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayLoggingFilterApplication.class, args);
+	}
+
+}
